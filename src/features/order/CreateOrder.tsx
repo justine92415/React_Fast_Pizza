2640b879915fa5a5c899372724061ed4b773aae0
justine92415 <justine92415@gmail.com>
2025-02-28 21:00:12 +0800

    feat(order):310.更新訂單頁面樣式以增強可用性

--- src/features/order/CreateOrder.tsx
+++ src/features/order/CreateOrder.tsx
@@ -42,32 +42,51 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
-        <div>
+        <div className="mb-5 flex flex-col gap-2">
           <label>First Name</label>
-          <input className="input" type="text" name="customer" required />
+          <input
+            className="input bg-white"
+            type="text"
+            name="customer"
+            required
+          />
         </div>
 
-        <div>
+        <div className="mb-5 flex flex-col gap-2">
           <label>Phone number</label>
-          <div>
-            <input className="input" type="tel" name="phone" required />
+          <div className="grow">
+            <input
+              className="input w-full bg-white"
+              type="tel"
+              name="phone"
+              required
+            />
+            {formErrors?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
+        <div className="mb-5 flex flex-col gap-2">
           <label>Address</label>
-          <div>
-            <input className="input" type="text" name="address" required />
+          <div className="grow">
+            <input
+              className="input w-full bg-white"
+              type="text"
+              name="address"
+              required
+            />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             className="h-6 w-6 accent-yellow-400 focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
@@ -76,12 +95,14 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label className="font-medium" htmlFor="priority">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type='primary'>
+          <Button disabled={isSubmitting} type="primary">
             {isSubmitting ? "Placeing order...." : "Order now"}
           </Button>
         </div>
--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -11,7 +11,7 @@ function Button({
   to?: string;
   type: "small" | "primary" | "secondary";
 }) {
-  const base = `inline-block rounded-full bg-yellow-400 font-semibold tracking-wide
+  const base = `inline-block text-sm rounded-full bg-yellow-400 font-semibold tracking-wide
         text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300
         focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none
         disabled:cursor-not-allowed disabled:bg-slate-600`;
@@ -20,7 +20,7 @@ function Button({
   } = {
     primary: base + " px-4 py-3",
     small: base + " px-4 py-2 text-xs",
-    secondary: `inline-block rounded-full border-2 border-stone-300 font-semibold tracking-wide
+    secondary: `inline-block text-sm rounded-full border-2 border-stone-300 font-semibold tracking-wide
         text-stone-400 uppercase transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800
         hover:focus:bg-stone-300 focus:text-stone-800 focus:ring focus:ring-stone-200 focus:ring-offset-2 focus:outline-none
         disabled:cursor-not-allowed disabled:bg-slate-600 px-4 py-2.5`,
