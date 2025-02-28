dbc81278f7729e7b5a64db98aa87ee577e9c4c4d
justine92415 <justine92415@gmail.com>
2025-02-28 17:31:06 +0800

    feat(order):290.新增訂單搜尋功能並整合至 Header

--- src/features/order/order.types.ts
+++ src/features/order/order.types.ts
@@ -2,11 +2,27 @@ export type OrderItemProps = {
   item: OrderItemType;
   isLoadingIngredients: boolean;
   ingredients: string[];
-}
+};
 
 export type OrderItemType = {
   id: number;
   name: string;
   quantity: number;
   totalPrice: number;
-}
+};
+
+export type OrderParams = {
+  params: {
+    orderId: number;
+  };
+};
+
+export type IOrder = {
+  id: string;
+  status: string;
+  priority: string;
+  priorityPrice: number;
+  orderPrice: number;
+  estimatedDelivery: string;
+  cart: string;
+};
